16d427c5fa1da51532adceec151dc7098d78d428
vasu <vasu2397@gmail.com>
2019-09-21 13:37:42 +0530

    Modified templates to display upload from database.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
+from .models import Upload
+from . import views
 # Create your views here.
 def Upload(request):
-    return render(request, 'blog/Upload.html', {})
+    
+    return render(request, 'blog/Upload.html', {'Upload':Upload})
+
+def objects(request):
+    upload = Upload.objects.order_by('name')
+    return render(request, 'blog/Upload.html', {'objects':objects})
