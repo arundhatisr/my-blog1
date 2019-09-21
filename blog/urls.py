773c451b3000671aa6b70322b6286eddbc2f0878
vasu <vasu2397@gmail.com>
2019-09-21 17:24:49 +0530

    Added view and template for detailed blog Upload as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+
+
 urlpatterns = [
     path('', views.Upload, name='Upload'),
-    path('', views.objects, name='objects'),
+
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,7 @@
 from django.shortcuts import render
 from .models import Upload
 from . import views
-# Create your views here.
-def Upload(request):
-    
-    return render(request, 'blog/Upload.html', {'Upload':Upload})
 
-def objects(request):
-    upload = Upload.objects.order_by('name')
-    return render(request, 'blog/Upload.html', {'objects':objects})
+
+def Upload(request):
+    return render(request, 'blog/Upload.html', {'Upload':Upload})
